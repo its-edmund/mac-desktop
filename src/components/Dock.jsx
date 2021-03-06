d170592f395075b01c88f96e18e54b5b1d040a1a
Edmund Xin <mxinburritos@gmail.com>
2021-03-06 01:29:38 -0800

    add time in toolbar

--- src/components/Dock.jsx
+++ src/components/Dock.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Box, Center, Image, Menu, MenuButton } from '@chakra-ui/react';
+import { Box, Center, Image, Menu, MenuButton, Button, Link, Tooltip } from '@chakra-ui/react';
 
 import launchpad from '../assets/launchpad.png';
+import github from '../assets/github.png';
 import settings from '../assets/settings.png';
 import calculator from '../assets/calculator.png';
 import terminal from '../assets/terminal.png';
@@ -14,13 +15,32 @@ const Dock = () => {
     <Box fixed mx='auto' bottom='0px' display='inline-block' backgroundColor='rgba(255,255,255,0.3)' mt='87vh' borderRadius='15px' style={{backdropFilter: 'blur(10px)'}}>
       <Center p={1}>
         <Menu>
-          <MenuButton><Image src={launchpad} width='50px' my='auto'/></MenuButton>
-          <MenuButton><Image src={messages} width='50px' my='auto'/></MenuButton>
-          <MenuButton><Image src={terminal} width='50px' my='auto'/></MenuButton>
-          <MenuButton><Image src={notepad} width='50px' my='auto'/></MenuButton>
-          <MenuButton><Image src={calculator} width='50px' my='auto'/></MenuButton>
-          <MenuButton><Image src={vscode} width='50px' my='auto'/></MenuButton>
-          <MenuButton><Image src={settings} width='50px' my='auto'/></MenuButton>
+          <Tooltip hasArrow label='Launchpad' bg="rgb(60,60,60)">
+            <MenuButton><Image src={launchpad} width='50px' my='auto'/></MenuButton>
+          </Tooltip>
+          <Tooltip hasArrow label='GitHub' bg="rgb(60,60,60)">
+            <Link href='https://github.com/mxinburritos' target='_blank'>
+              <Button display='contents'><Image src={github} width='50px' my='auto'/></Button>
+            </Link>
+          </Tooltip>
+          <Tooltip hasArrow label='Messages' bg="rgb(60,60,60)">
+            <MenuButton><Image src={messages} width='50px' my='auto'/></MenuButton>
+          </Tooltip>
+          <Tooltip hasArrow label='Terminal' bg="rgb(60,60,60)">
+            <MenuButton><Image src={terminal} width='50px' my='auto'/></MenuButton>
+          </Tooltip>
+          <Tooltip hasArrow label='Notepad' bg="rgb(60,60,60)">
+            <MenuButton><Image src={notepad} width='50px' my='auto'/></MenuButton>
+          </Tooltip>
+          <Tooltip hasArrow label='Calculator' bg="rgb(60,60,60)">
+            <MenuButton><Image src={calculator} width='50px' my='auto'/></MenuButton>
+          </Tooltip>
+          <Tooltip hasArrow label='Visual Studio Code' bg="rgb(60,60,60)">
+            <MenuButton><Image src={vscode} width='50px' my='auto'/></MenuButton>
+          </Tooltip>
+          <Tooltip hasArrow label='System Preferences' bg="rgb(60,60,60)">
+            <MenuButton><Image src={settings} width='50px' my='auto'/></MenuButton>
+          </Tooltip>
         </Menu>
       </Center>
     </Box>
--- src/components/Toolbar.jsx
+++ src/components/Toolbar.jsx
@@ -1,15 +1,54 @@
-import { Box, Stack } from '@chakra-ui/layout';
-import { Menu, MenuButton } from '@chakra-ui/menu';
+import React, {useEffect, useState} from 'react';
+import { Box, Stack, Divider, Text } from '@chakra-ui/layout';
+import { Menu, MenuButton, MenuList, MenuItem } from '@chakra-ui/menu';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faApple } from '@fortawesome/free-brands-svg-icons';
-import React from 'react';
+import { Spacer } from '@chakra-ui/layout';
+
+const getClock = () => {
+  const two = (x) => x < 10 ? `0${x}` : x;
+  const date = new Date();
+  const D = date.getDay();
+  const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+  const M = date.getMonth();
+  const months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+  const H = date.getHours();
+  const m = date.getMinutes();
+  const hh = H % 12 || 12;
+  const mm = two(m);
+  const A = ['AM', 'PM'][H / 12 | 0];
+  return `${days[D]} ${months[M]} ${hh}:${mm} ${A}`;
+};
 
 const Toolbar = () => {
+  const [time, setTime] = useState(getClock());
+
+  useEffect(() => {
+    const interval = window.setInterval(() => {
+      const clock = getClock();
+      setTime(clock);
+    }, 1000);
+
+    return () => {
+      window.clearInterval(interval);
+    };
+  }, []);
+
   return (
     <Box backgroundColor='rgba(255,255,255,0.3)' style={{backdropFilter: 'blur(10px)'}}>
       <Stack direction='row' spacing={5} ml={5}>
         <Menu>
           <MenuButton><FontAwesomeIcon icon={faApple}/></MenuButton>
+          <MenuList backgroundColor='rgba(255,255,255,0.3)' ml={-8} mt={-1} border='none'>
+            <MenuItem>About This Mac</MenuItem>
+            <MenuItem>System Preferences</MenuItem>
+            <Divider orientation="horizontal" w='90%' color='rgba(0,0,0,0.3)' />
+            <MenuItem>Sleep</MenuItem>
+            <MenuItem>Restart...</MenuItem>
+            <MenuItem>Shut Down...</MenuItem>
+          </MenuList>
+        </Menu>
+        <Menu>
           <MenuButton fontWeight='bold'>Finder</MenuButton>
           <MenuButton>File</MenuButton>
           <MenuButton>Edit</MenuButton>
@@ -18,6 +57,8 @@ const Toolbar = () => {
           <MenuButton>Window</MenuButton>
           <MenuButton>Help</MenuButton>
         </Menu>
+        <Spacer />
+        <Text mr={5}>{time}</Text>
       </Stack>
     </Box>
   );
