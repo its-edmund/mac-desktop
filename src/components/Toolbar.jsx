ac573a1429e39e40f6402702fa4bde704b90e7c8
Edmund Xin <mxinburritos@gmail.com>
2021-03-06 01:58:37 -0800

    add icons to the toolbar

--- src/components/Toolbar.jsx
+++ src/components/Toolbar.jsx
@@ -1,9 +1,13 @@
 import React, {useEffect, useState} from 'react';
+// import { Image } from '@chakra-ui/react';
 import { Box, Stack, Divider, Text } from '@chakra-ui/layout';
 import { Menu, MenuButton, MenuList, MenuItem } from '@chakra-ui/menu';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faApple } from '@fortawesome/free-brands-svg-icons';
+import { faWifi, faVolumeMute, faBatteryHalf } from '@fortawesome/free-solid-svg-icons';
 import { Spacer } from '@chakra-ui/layout';
+
+// import wifi from '../assets/wifi.png';
 
 const getClock = () => {
   const two = (x) => x < 10 ? `0${x}` : x;
@@ -12,12 +16,13 @@ const getClock = () => {
   const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
   const M = date.getMonth();
   const months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+  const d = date.getDate();
   const H = date.getHours();
   const m = date.getMinutes();
   const hh = H % 12 || 12;
   const mm = two(m);
   const A = ['AM', 'PM'][H / 12 | 0];
-  return `${days[D]} ${months[M]} ${hh}:${mm} ${A}`;
+  return `${days[D]} ${months[M]} ${d} ${hh}:${mm} ${A}`;
 };
 
 const Toolbar = () => {
@@ -58,6 +63,11 @@ const Toolbar = () => {
           <MenuButton>Help</MenuButton>
         </Menu>
         <Spacer />
+        <Menu>
+          <MenuButton><FontAwesomeIcon icon={faVolumeMute} /></MenuButton>
+          <MenuButton><FontAwesomeIcon icon={faBatteryHalf} /></MenuButton>
+          <MenuButton><FontAwesomeIcon icon={faWifi} /></MenuButton>
+        </Menu>
         <Text mr={5}>{time}</Text>
       </Stack>
     </Box>
